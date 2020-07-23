876138faafbfe036a3f9bd6343f407a94484ba2d
Talal Alothman <stenfeio@gmail.com>
2020-07-23 18:57:00 -0400

    Handle orienting plane

--- Assets/Scripts/MouseInteraction.cs
+++ Assets/Scripts/MouseInteraction.cs
@@ -3,15 +3,31 @@ using UnityEngine.InputSystem;
 
 public class MouseInteraction : MonoBehaviour
 {
+    [SerializeField]
+    private Transform cursorObject;
+
     private Camera mainCamera;
+    private int layerMask = 1 << 8;
     
+    private Ray rayIntoWorld;
+    private RaycastHit raycastHit;
+    private Vector2 mousePosition;
+
     private void Start()
     {
         mainCamera = Camera.main;
+        mainCamera.depthTextureMode = DepthTextureMode.DepthNormals;
     }
 
     private void Update()
     {
-        print(Mouse.current.position.ReadValue());
+        mousePosition = Mouse.current.position.ReadValue();
+        rayIntoWorld = mainCamera.ScreenPointToRay(mousePosition);
+
+        if(Physics.Raycast(rayIntoWorld, out raycastHit, layerMask)) {
+            cursorObject.position = raycastHit.point;
+            cursorObject.rotation = Quaternion.FromToRotation(-Vector3.forward, raycastHit.normal);
+        }
+
     }
 }
