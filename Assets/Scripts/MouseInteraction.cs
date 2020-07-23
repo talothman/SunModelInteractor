ceeffd7f5687f24030d82bd5c03d05aa7e44c5f8
Talal Alothman <stenfeio@gmail.com>
2020-07-23 15:20:22 -0400

    Add mouseInteraction script

---
+++ Assets/Scripts/MouseInteraction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseInteraction : MonoBehaviour
+{
+    private Camera mainCamera;
+    
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        print(Mouse.current.position.ReadValue());
+    }
+}
