61ceb1e5dd499c3da19ff7d714e17afad89910b0
Talal Alothman <stenfeio@gmail.com>
2020-07-24 17:39:53 -0400

    Add scale function + hide plane

--- Assets/Scripts/MouseInteraction.cs
+++ Assets/Scripts/MouseInteraction.cs
@@ -5,6 +5,9 @@ public class MouseInteraction : MonoBehaviour
 {
     [SerializeField]
     private Transform cursorObject;
+    [SerializeField]
+    private Vector3 positionOffset;
+    private MeshRenderer cursorObjectRenderer;
 
     private Camera mainCamera;
     private int layerMask = 1 << 8;
@@ -15,19 +18,43 @@ public class MouseInteraction : MonoBehaviour
 
     private void Start()
     {
+        cursorObjectRenderer = cursorObject.GetComponent<MeshRenderer>();
         mainCamera = Camera.main;
         mainCamera.depthTextureMode = DepthTextureMode.DepthNormals;
+        cursorObjectRenderer.enabled = false;
     }
 
     private void Update()
     {
+        CheckScale();
         mousePosition = Mouse.current.position.ReadValue();
         rayIntoWorld = mainCamera.ScreenPointToRay(mousePosition);
 
-        if(Physics.Raycast(rayIntoWorld, out raycastHit, layerMask)) {
-            cursorObject.position = raycastHit.point;
-            cursorObject.rotation = Quaternion.FromToRotation(-Vector3.forward, raycastHit.normal);
+        if (Mouse.current.leftButton.isPressed) {
+            if(Physics.Raycast(rayIntoWorld, out raycastHit, layerMask)) {
+                cursorObjectRenderer.enabled = true;
+                cursorObject.position = raycastHit.point + Vector3.Project(positionOffset, raycastHit.normal);
+                Quaternion targetRotation = Quaternion.FromToRotation(-Vector3.forward, raycastHit.normal);
+                cursorObject.rotation = Quaternion.RotateTowards(cursorObject.rotation, targetRotation, 1);
+            } else {
+                cursorObjectRenderer.enabled = false;
+            }
         }
+    }
 
+    private void CheckScale() {
+        if (!Mouse.current.rightButton.wasPressedThisFrame && !Mouse.current.rightButton.wasReleasedThisFrame) {
+            if (Mouse.current.rightButton.isPressed) {
+                cursorObject.localScale += Vector3FromScalar(Mouse.current.delta.ReadValue().x)*Time.deltaTime;
+            }
+        }
+    }
+
+    private Vector3 Vector3FromScalar(float scaler) {
+        return new Vector3(scaler, scaler, scaler);
+    }
+
+    private Vector2 Vector2FromScalar(float scaler) {
+        return new Vector2(scaler, scaler);
     }
 }
