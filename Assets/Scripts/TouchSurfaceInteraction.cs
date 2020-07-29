f3d54b74ee8486987388e564bd04e09da24c52a5
Talal Alothman <stenfeio@gmail.com>
2020-07-29 10:44:09 -0400

    Add TouchSurfaceInteraction

---
+++ Assets/Scripts/TouchSurfaceInteraction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TouchSurfaceInteraction : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cursorObject;
+    [SerializeField]
+    private Vector3 positionOffset;
+    private MeshRenderer cursorObjectRenderer;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
