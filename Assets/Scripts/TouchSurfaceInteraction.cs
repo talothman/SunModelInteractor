5691020a3201492eef7a1722ea7f16c533f586e2
Talal Alothman <stenfeio@gmail.com>
2020-07-29 11:27:36 -0400

    Add collision detection for controllers

--- Assets/Scripts/TouchSurfaceInteraction.cs
+++ Assets/Scripts/TouchSurfaceInteraction.cs
@@ -8,13 +8,26 @@ public class TouchSurfaceInteraction : MonoBehaviour
     private Vector3 positionOffset;
     private MeshRenderer cursorObjectRenderer;
 
+    private int layerMask = 1 << 8;
+
     private void Start()
     {
-        
+        cursorObjectRenderer = cursorObject.GetComponent<MeshRenderer>();
+        cursorObjectRenderer.enabled = false;
     }
 
-    private void Update()
-    {
-        
+    private void Update() {
+
+    }
+    
+    private void OnCollisionStay(Collision other) {
+        if(other.gameObject.layer == layerMask) {
+            cursorObjectRenderer.enabled = true;
+            cursorObject.position = other.GetContact(0).point + Vector3.Project(positionOffset, other.GetContact(0).normal);
+            Quaternion targetRotation = Quaternion.FromToRotation(-Vector3.forward, other.GetContact(0).normal);
+            cursorObject.rotation = Quaternion.RotateTowards(cursorObject.rotation, targetRotation, 3);
+        } else {
+            cursorObjectRenderer.enabled = false;
+        }
     }
 }
