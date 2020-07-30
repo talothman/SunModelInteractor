51d80edacad88ceb22508a7bb2ab4f3719ac2f3c
Talal Alothman <stenfeio@gmail.com>
2020-07-29 23:54:38 -0400

    Replace collider check with raycast check

--- Assets/Scripts/TouchSurfaceInteraction.cs
+++ Assets/Scripts/TouchSurfaceInteraction.cs
@@ -6,9 +6,15 @@ public class TouchSurfaceInteraction : MonoBehaviour
     private Transform cursorObject;
     [SerializeField]
     private Vector3 positionOffset;
+    [SerializeField]
+    private Transform modelTransform;
+    [SerializeField]
+    private Transform raySource;
     private MeshRenderer cursorObjectRenderer;
 
     private int layerMask = 1 << 8;
+    private Ray controllerToModelRay;
+    private RaycastHit controllerToModelRaycastHit;
 
     private void Start()
     {
@@ -17,17 +23,32 @@ public class TouchSurfaceInteraction : MonoBehaviour
     }
 
     private void Update() {
-
+        //OVRInput.Axis1D()
+        CheckRaycastToObject();
     }
-    
-    private void OnCollisionStay(Collision other) {
-        if(other.gameObject.layer == layerMask) {
+
+    // private void OnCollisionStay(Collision other) {
+    //     if((1 << other.gameObject.layer) == layerMask) {
+    //         cursorObjectRenderer.enabled = true;
+    //         cursorObject.position = other.collider.ClosestPoint(transform.position) + Vector3.Project(positionOffset, other.GetContact(0).normal);
+    //         Quaternion targetRotation = Quaternion.FromToRotation(-Vector3.forward, other.GetContact(0).normal);
+    //         cursorObject.rotation = Quaternion.RotateTowards(cursorObject.rotation, targetRotation, 50);
+    //     }
+    // }
+
+    private void CheckRaycastToObject() {      
+
+        controllerToModelRay = new Ray(transform.position, modelTransform.position-transform.position);
+        if (Physics.Raycast(controllerToModelRay, out controllerToModelRaycastHit, 10f, layerMask)) {
+            if (Vector3.Distance(controllerToModelRaycastHit.point, transform.position) > 0.1f) {
+                cursorObjectRenderer.enabled = false;
+                return;
+            }
+
             cursorObjectRenderer.enabled = true;
-            cursorObject.position = other.GetContact(0).point + Vector3.Project(positionOffset, other.GetContact(0).normal);
-            Quaternion targetRotation = Quaternion.FromToRotation(-Vector3.forward, other.GetContact(0).normal);
-            cursorObject.rotation = Quaternion.RotateTowards(cursorObject.rotation, targetRotation, 3);
-        } else {
-            cursorObjectRenderer.enabled = false;
+            cursorObject.position = controllerToModelRaycastHit.point + Vector3.Project(positionOffset, controllerToModelRaycastHit.normal);
+            Quaternion targetRotation = Quaternion.FromToRotation(-Vector3.forward, controllerToModelRaycastHit.normal);
+            cursorObject.rotation = Quaternion.RotateTowards(cursorObject.rotation, targetRotation, 50);
         }
     }
 }
