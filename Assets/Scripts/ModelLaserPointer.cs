77cfead70e8d3e4bcb0dfe31e671b16e6f207b5f
Talal Alothman <stenfeio@gmail.com>
2020-08-03 16:03:09 -0400

    Add ModelLaserPointer component

---
+++ Assets/Scripts/ModelLaserPointer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ModelLaserPointer : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private Vector3[] positions = new Vector3[2];
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+    }
+
+    private void LateUpdate()
+    {
+        lineRenderer.SetPositions(positions);
+    }
+
+    public void SetLinePositions(Vector3 newStart, Vector3 newEnd) {
+        positions[0] = newStart;
+        positions[1] = newEnd;
+    }
+
+    public void SetPointerVisability(bool on) {
+        lineRenderer.enabled = on;
+    }
+}
